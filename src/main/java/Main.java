c769cb0a89528f71754ebf631e60896ba464373a
ulak-github <aayushkaulak@gmail.com>
2025-11-28 16:33:20 +0100

    Person class
    properties and methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ import javax.xml.transform.Source;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hey");
+        Person a = new Person("Aaliyah", "Kathmandu", "061552265");
+        a.setiD("12345");
 
+        System.out.println(a.getInfos());
     }
 }
---
+++ src/main/java/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private String iD;
+    private String name;
+    private String address;
+    private String contact;
+
+    public Person(String name, String address, String contact){
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String getContact(){
+        return contact;
+    }
+
+    public void setiD(String iD){
+        this.iD=iD;
+    }
+
+    public String getInfos(){
+        return "ID: " + iD +
+                ", Name: " + name +
+                ", Address: " + address +
+                ", Contact: " + contact;
+    }
+
+}
