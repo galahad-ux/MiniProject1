d3ccbeaf58f396dc5098f18acba717bafbb02007
galahad-ux <milla.vien@gmail.com>
2025-11-28 17:24:23 +0100

    created Flight class
    created Flight class and started a dictionary to store the flights' info (might switch it to an enumeration class later)

---
+++ src/main/java/Flight.java
@@ -0,0 +1,23 @@
+public class Flight {
+    private String flightNumber;
+    private String origin;
+    private String destination;
+    private String departureTime;
+    private String arrivalDateTime;
+    private String status;
+
+    public Flight(String flightNumber, String origin, String destination, String departureTime, String arrivalDateTime, String status){
+        flightNumber = this.flightNumber;
+        origin = this.origin;
+        destination = this.destination;
+        departureTime = this.departureTime;
+        arrivalDateTime = this.arrivalDateTime;
+        status = this.status;
+    }
+
+    public
+
+    public String getFlightNumber(){
+        return flightNumber
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import javax.xml.transform.Source;
 
 public class Main {
-
+    public class Flight{
+        String flightNumber
     public static void main(String[] args) {
-        System.out.println("hey");
+        public class Flight{
+            String flightNumber
+        }
 
     }
+
+
 }
