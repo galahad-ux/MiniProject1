936007b273c0ffa91e30c61303da40eb3f88868d
ulak-github <aayushkaulak@gmail.com>
2025-12-11 21:13:57 +0100

    csv methods employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee extends Person {
     public String numberEmp;
     private String hiringDate;
@@ -23,5 +27,45 @@ public class Employee extends Person {
 
     public String getRole(){
         return "Role: " + role;
+    }
+
+    //CSV methods
+    public String toCSV(){
+        return getId() + "," + getName() + "," + getAddress() + "," + getContact() + "," + role + "," + numberEmp
+                + "," + hiringDate;
+    }
+
+    public static Employee fromCSV(String csvLine){
+        String[] objects = csvLine.split(",");
+        RoleIdentifier role = RoleIdentifier.valueOf(objects[4]);
+        return new Employee(objects[0], objects[1], objects[2], objects[3], role, objects[5], objects[6]);
+    }
+
+    public static void saveAllEmployees(List<Employee> employees ){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("employees.csv"))){
+            for (Employee e : employees){
+                writer.write(e.toCSV());
+                writer.newLine();
+            }
+            System.out.println("Saved to file");
+        } catch (IOException e) {
+            System.out.println("Error! can't save employee" + e.getMessage());
+        }
+    }
+
+    public static List<Employee> loadAllEmployees(){
+        List<Employee> employees = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("employee.csv"))){
+            String line;
+            while((line = reader.readLine()) != null){
+                if (!line.trim().isEmpty()){
+                    employees.add(fromCSV(line));
+                }
+            }
+            System.out.println("Loaded passenger");
+        }catch (IOException e){
+            System.out.println("Error! Can't load employees" + e.getMessage());
+        }
+        return employees;
     }
 }
