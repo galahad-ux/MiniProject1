dfd7e2166696b558cbf93742b21a596e06af3f59
ulak-github <aayushkaulak@gmail.com>
2025-12-11 22:19:15 +0100

    csv for reservations

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,5 +50,70 @@ public class Book {
             }
         }
         return reserves;
+    }
+
+    //CSV methods
+    public String toCSV(){
+        String flightNum = "";
+        for (Flight f: reservedFlights){
+            if (!flightNum.isEmpty()){
+                flightNum += ";";           //flights seperated by ;
+            }
+            flightNum += f.getFlightNumber();
+        }
+        if (flightNum.isEmpty()){
+            flightNum = "None";     //no flights booked
+        }
+        return reservationNumber + "," + passenger.getId() + "," + flightNum + "," + status;
+    }
+
+    public static Book fromCSV(String csvLine, List<Passenger> allBookedPassengers, List<Flight> allFlights){
+        String[] objects = csvLine.split(",");
+        String reservationId = objects[0];
+        String passengerId = objects[1];
+        String flightNumbers = objects[2];
+        Status status = Status.valueOf(objects[3]);
+
+        //with passengerid we want the passenger and with flight id we want the flight that was booked
+        Passenger foundP = null;
+        for (Passenger p : allBookedPassengers){
+            if (p.getId().equals(passengerId)){
+                foundP = p;
+                break;
+            }
+        }
+
+        Book book = new Book(reservationId,foundP);
+        System.out.println("booking created for" + passengerId);
+        book.modifyReservation(status);
+        return book;
+    }
+
+    public static void saveAllBooks(List<Book> books){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("reservations.csv"))){
+            for (Book b : books){
+                writer.write(b.toCSV());
+                writer.newLine();
+            }
+            System.out.println("Saved the reservations");
+        } catch (IOException e) {
+            System.out.println("Error! can't save reservation" + e.getMessage());
+        }
+    }
+
+    public static List<Book> loadAllBooks (List<Passenger> passengers, List<Flight> flights){
+        List<Book> books = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("reservations.csv"))){
+            String line;
+            while((line = reader.readLine()) != null){
+                if (!line.trim().isEmpty()){
+                    books.add(fromCSV(line, passengers, flights));
+                }
+            }
+            System.out.println("Loaded reservations from csv");
+        }catch (IOException e){
+            System.out.println("Error! Can't load reservations from csv" + e.getMessage());
+        }
+        return books;
     }
 }
